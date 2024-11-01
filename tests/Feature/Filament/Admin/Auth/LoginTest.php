0161405e0586dda9cf4150c2cabe7b15863c8f83
chester <chestersykes1@gmail.com>
2024-11-01 23:22:28 +0800

    Login tests added

---
+++ tests/Feature/Filament/Admin/Auth/LoginTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Models\User;
+use Filament\Pages\Auth\Login;
+use Illuminate\Support\Facades\Hash;
+
+use function Pest\Livewire\livewire;
+use function PHPUnit\Framework\assertEquals;
+
+it('can login with valid credentials', function () {
+    $user = User::factory()->create([
+        'password' => Hash::make('password'),
+    ]);
+
+    livewire(Login::class)
+        ->fillForm([
+            'email' => $user->email,
+            'password' => 'password',
+        ])
+        ->call('authenticate')
+        ->assertHasNoFormErrors();
+
+    assertEquals($user->id, auth()->id());
+});
+
+it('does not login with invalid credentials', function () {
+    $user = User::factory()->create([
+        'password' => Hash::make('password'),
+    ]);
+
+    livewire(Login::class)
+        ->fillForm([
+            'email' => $user->email,
+            'password' => 'password-wrong',
+        ])
+        ->call('authenticate')
+        ->assertHasFormErrors(['email']);
+
+    assertEquals(null, auth()->id());
+});
+
+it('does not login a user with an unverified email', function () {
+    $user = User::factory()->unverified()->create([
+        'password' => Hash::make('password'),
+    ]);
+
+    livewire(Login::class)
+        ->fillForm([
+            'email' => $user->email,
+            'password' => 'password',
+        ])
+        ->call('authenticate')
+        ->assertHasFormErrors(['email']);
+
+    assertEquals(null, auth()->id());
+});
