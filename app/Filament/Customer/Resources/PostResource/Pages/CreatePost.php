5e635e66475177ba993d256f8af72cf1e92fb9bb
chester <chestersykes1@gmail.com>
2024-11-10 13:06:56 +0800

    added the 'published_at' and 'user_id'

--- app/Filament/Customer/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Customer/Resources/PostResource/Pages/CreatePost.php
@@ -8,4 +8,12 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['published_at'] = $data['published_at'] ? now()->format('Y-m-d H:i:s') : null;
+        $data['user_id'] = auth()->id();
+
+        return $data;
+    }
 }
